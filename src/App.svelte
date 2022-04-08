b28ea514aeb73be59c4d51f0d79c8f2c1b8f3d4c
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-04-09 05:57:56 +0700

    Update Input

--- src/App.svelte
+++ src/App.svelte
@@ -11,24 +11,27 @@
    }
 </script>
 
-<main class="container mx-auto">
+<main class="container mx-auto mt-10">
   
 <div class="flex w-full gap-2">
-  <div class="bg-sky-500 w-full p-2">
-    <div class="flex flex-col w-1/2 gap-2">
-      
+  <div class="bg-white rounded-md shadow-lg w-2/5 p-2">
+    <div class="flex flex-col w-full gap-2">
+      <div class="flex justify-between items-center w-full border-b-2 border-slate-500 py-2">
+        <h1 class="text-[20px] font-semibold uppercase">Prediksi</h1>
+        <button on:click={() => {
+          callPrediksi();
+        }} class="bg-primary p-2 rounded-md text-white shadow-lg">CHECK</button>
+      </div>
       <input 
         type="text" 
         placeholder="Type here" 
-        class="input input-bordered rounded-sm w-full">
+        class="input input-bordered focus:ring-0 focus:outline-0 rounded-sm w-full">
         <SveltyPicker 
           inputClasses="input input-bordered rounded-sm w-full text-center" format="yyyy-mm-dd" bind:value={myDate}></SveltyPicker>
         <SveltyPicker 
           inputClasses="input input-bordered rounded-sm w-full text-center" format="hh:ii" mode="time" bind:value={myTime}></SveltyPicker>
         
-        <button on:click={() => {
-          callPrediksi();
-      }} class="btn btn-primary">CLick Me</button>
+        <button  class="btn btn-primary">CLick Me</button>
     </div>
   </div>
   <div class="bg-amber-500 w-full p-2">
